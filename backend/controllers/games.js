3e2a3287a73882113e814b6a026ca3faab4204e8
tristhaus <tristhaus@yahoo.de>
2023-03-14 13:35:31 +0100

    Add GET endpoints for reader, writer

--- backend/controllers/games.js
+++ backend/controllers/games.js
@@ -1,5 +1,6 @@
 const gamesRouter = require('express').Router()
 const Game = require('../models/game')
+const logger = require('../utils/logger')
 
 const dataVersion = 1
 
@@ -35,6 +36,47 @@ gamesRouter.post('/', async (request, response, next) => {
         }
 
         response.status(201).json(apiModel)
+    } catch (error) {
+        next(error)
+    }
+})
+
+// for reader
+gamesRouter.get('/:readerId', async (request, response, next) => {
+    try {
+        const game = await Game.findOne({ readerId: request.params.readerId })
+
+        if (!game) {
+            logger.error(`game with readerId '${request.params.readerId}' not found`)
+            response.status(404).send({ error: `readerId not found: ${request.params.readerId}` })
+        }
+
+        const apiModel = {
+            'readerId': game.readerId,
+        }
+
+        response.status(200).json(apiModel)
+    } catch (error) {
+        next(error)
+    }
+})
+
+// for writer
+gamesRouter.get('/write/:writerId', async (request, response, next) => {
+    try {
+        const game = await Game.findOne({ writerId: request.params.writerId })
+
+        if (!game) {
+            logger.error(`game with writerId '${request.params.writerId}' not found`)
+            response.status(404).send({ error: `writerId not found: ${request.params.writerId}` })
+        }
+
+        const apiModel = {
+            'readerId': game.readerId,
+            'writerId': game.writerId,
+        }
+
+        response.status(200).json(apiModel)
     } catch (error) {
         next(error)
     }
--- backend/tests/game_api.test.js
+++ backend/tests/game_api.test.js
@@ -1,16 +1,82 @@
 const supertest = require('supertest')
 const app = require('../app')
+const Game = require('../models/game')
 
 const api = supertest(app)
+
+beforeAll(async () => {
+    await Game.deleteMany({})
+})
 
 describe('game API', () => {
 
     test('POST creates new game', async () => {
         const response = await api.post('/api/game')
 
+        expect(response.status).toBe(201)
         expect(response.body).toBeDefined()
         expect(response.body.writerId).toBeDefined()
         expect(response.body.readerId).toBeDefined()
+    })
+
+    test('Game is accessible to readers by reader ID', async () => {
+        const creationResponse = await api.post('/api/game')
+
+        expect(creationResponse.status).toBe(201)
+        expect(creationResponse.body).toBeDefined()
+
+        const readerId = creationResponse.body.readerId
+        const readGamePath = `/api/game/${readerId}`
+
+        const response = await api.get(readGamePath)
+
+        expect(response.status).toBe(200)
+        expect(response.body.writerId).not.toBeDefined()
+        expect(response.body.readerId).toBeDefined()
+    })
+
+    test('Game is not accessible to readers by writer ID', async () => {
+        const creationResponse = await api.post('/api/game')
+
+        expect(creationResponse.status).toBe(201)
+        expect(creationResponse.body).toBeDefined()
+
+        const writerId = creationResponse.body.writerId
+        const readGamePath = `/api/game/${writerId}`
+
+        const response = await api.get(readGamePath)
+
+        expect(response.status).toBe(404)
+    })
+
+    test('Game is accessible to writers by writer ID', async () => {
+        const creationResponse = await api.post('/api/game')
+
+        expect(creationResponse.status).toBe(201)
+        expect(creationResponse.body).toBeDefined()
+
+        const writerId = creationResponse.body.writerId
+        const writeGamePath = `/api/game/write/${writerId}`
+
+        const response = await api.get(writeGamePath)
+
+        expect(response.status).toBe(200)
+        expect(response.body.writerId).toBeDefined()
+        expect(response.body.readerId).toBeDefined()
+    })
+
+    test('Game is not accessible to writers by reader ID', async () => {
+        const creationResponse = await api.post('/api/game')
+
+        expect(creationResponse.status).toBe(201)
+        expect(creationResponse.body).toBeDefined()
+
+        const readerId = creationResponse.body.readerId
+        const writeGamePath = `/api/game/write/${readerId}`
+
+        const response = await api.get(writeGamePath)
+
+        expect(response.status).toBe(404)
     })
 
 })
